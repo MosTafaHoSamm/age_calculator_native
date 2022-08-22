88375cf785025dd34e24f856a754eee1339809f6
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-08-22 16:19:44 +0200

    relatie layout

--- app/src/main/java/com/mostafa/agecalculator/MainActivity.kt
+++ app/src/main/java/com/mostafa/agecalculator/MainActivity.kt
@@ -11,16 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
 
-
-            try {
-                val birth: Int = editTextNumber.text.toString().toInt()
-                val age: Int = Calendar.getInstance().get(Calendar.YEAR) - birth
-                textView.text = age.toString()
-                println("Done Done ")
-            } catch (e: NumberFormatException) {
-                println("Error : $e")
 
 
             }
@@ -28,5 +19,4 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    }
-}
+
