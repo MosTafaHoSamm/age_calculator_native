8962f5606a1778ad70c35488161ce4d3fc74def0
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-08-22 11:51:22 +0200

    first commit

---
+++ app/src/main/java/com/mostafa/agecalculator/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.mostafa.agecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+
+
+            try {
+                val birth: Int = editTextNumber.text.toString().toInt()
+                val age: Int = Calendar.getInstance().get(Calendar.YEAR) - birth
+                textView.text = age.toString()
+                println("Done Done ")
+            } catch (e: NumberFormatException) {
+                println("Error : $e")
+            }
+
+
+        }
+
+    }
+}
